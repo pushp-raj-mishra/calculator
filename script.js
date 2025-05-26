03fcb3b6803ca36df54535bcdaba4995799a72c1
pushp-raj-mishra <69637880+pushp-raj-mishra@users.noreply.github.com>
2025-05-26 17:33:29 +0530

    Add basic calculator functionality

---
+++ script.js
@@ -0,0 +1,78 @@
+
+
+let display = document.querySelector(".display-number");
+let clear = true;
+function handle(arr,elem){
+    arr.push(elem);
+    if (clear) {
+        display.textContent = "";
+        clear = false;
+    }
+    display.textContent += (" "+elem);
+}
+
+let work = [];
+let zero = document.querySelector(".zero");
+zero.addEventListener("click",()=>handle(work,0));
+
+let one = document.querySelector(".one");
+one.addEventListener("click",()=>handle(work,1));
+
+let two = document.querySelector(".two");
+two.addEventListener("click",()=>handle(work,2));
+
+let three = document.querySelector(".three");
+three.addEventListener("click",()=>handle(work,3));
+
+let four = document.querySelector(".four");
+four.addEventListener("click",()=>handle(work,4));
+
+let five = document.querySelector(".five");
+five.addEventListener("click",()=>handle(work,5));
+
+let six = document.querySelector(".six");
+six.addEventListener("click",()=>handle(work,6));
+
+let seven = document.querySelector(".seven");
+seven.addEventListener("click",()=>handle(work,7));
+
+let eight = document.querySelector(".eight");
+eight.addEventListener("click",()=>handle(work,8));
+
+let nine = document.querySelector(".nine");
+nine.addEventListener("click",()=>handle(work,9));
+
+let ops = [];
+
+let add = document.querySelector(".add");
+add.addEventListener("click",()=>handle(ops,'+'));
+
+let sub = document.querySelector(".sub");
+sub.addEventListener("click",()=>handle(ops,'-'));
+
+let mul = document.querySelector(".mul");
+mul.addEventListener("click",()=>handle(ops,'*'));
+
+let div = document.querySelector(".div");
+div.addEventListener("click",()=>handle(ops,'/'));
+
+let equal = document.querySelector(".equal");
+equal.addEventListener("click",calculate);
+
+
+function calculate(){
+    let ans = 0;
+    if (ops[0]=='+') {
+        ans = work[0]+work[1];
+    } else if (ops[0]=='-') {
+        ans = work[0]-work[1];
+    } else if (ops[0]=='*') {
+        ans = work[0]*work[1];
+    } else {
+        ans = work[0]/work[1];
+    }
+    display.textContent = ans;
+    clear = true;
+}
+
+
